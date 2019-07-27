4115ee4de181aa62c08e90f83edcc19cfc94719e
Jag <jagdeepsingh3939@gmail.com>
2019-07-26 20:50:46 -0700

    fix incorrect path in create-user

--- src/app-logic/create-user/create-user.js
+++ src/app-logic/create-user/create-user.js
@@ -1,4 +1,4 @@
-const queryDB = require('../database-interactions/query-postgres');
+const queryDB = require('../../database-interactions/query-postgres');
 
 const ADD_USER_QUERY = `INSERT INTO users (user_id, display_name, user_handle, photo_link, token, token_secret) VALUES ($1, $2, $3, $4, $5, $6) ON CONFLICT (user_id) DO UPDATE SET display_name = $2, user_handle = $3, photo_link = $4, token = $5, token_secret = $6 RETURNING (user_id, display_name, user_handle);`;
 
