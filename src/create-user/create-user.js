0da32970ef30ab073e2e526b6d55ae5c0fac7705
Jag <jagdeepsingh3939@gmail.com>
2019-07-26 19:20:17 -0700

    on conflict insert

--- src/create-user/create-user.js
+++ src/create-user/create-user.js
@@ -1,6 +1,6 @@
 const queryDB = require('../database-interactions/query-postgres');
 
-const ADD_USER_QUERY = `INSERT INTO users (user_id, display_name, user_handle, photo_link, token, token_secret) VALUES ($1, $2, $3, $4, $5, $6) RETURNING (user_id, display_name, user_handle, photo_link);`;
+const ADD_USER_QUERY = `INSERT INTO users (user_id, display_name, user_handle, photo_link, token, token_secret) VALUES ($1, $2, $3, $4, $5, $6) ON CONFLICT (user_id) DO NOTHING RETURNING (user_id, display_name, user_handle, photo_link);`;
 
 module.exports = (userData) => {
   const userDataForQuery = [
